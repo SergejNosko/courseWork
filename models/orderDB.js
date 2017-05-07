22cec246e55deaf52379ade32c6d1724e852979b
SergejNosko <tuzemec81@gmail.com>
2017-05-07 10:40:12 +0300

    Public my course work

---
+++ models/orderDB.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Сергей Носко on 03.05.2017.
+ */
+var crypto = require('crypto');
+var async = require('async');
+var util = require('util');
+
+var mongoose = require('../libs/mongoose'),
+    Schema = mongoose.Schema;
+
+var order = new Shema({
+
+});
